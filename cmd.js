930f32f14db645ef58bc9f77e13e33e902d8f159
Leonardo Marostegam <82916289+leomarostegam@users.noreply.github.com>
2025-07-31 09:09:18 -0300

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,3 +1,1 @@
-{ 
-  "u": "eyJjb21hbmRvIjoiYnJpZGdlIiwiYnJpZGdlIjp7InR5cGUiOiJ3ZWJzb2NrZXQiLCJzZXJ2ZXIiOiJ3c3M6Ly93d3cucmVkdGVhbXNlYy54eXovd3MiLCJsb2NhbFBvcnQiOjg3NjV9fQo=" 
-}
+{ "u": "eyJjb21hbmRvIjoiYnJpZGdlIiwiYnJpZGdlIjp7InR5cGUiOiJ3ZWJzb2NrZXQiLCJzZXJ2ZXIiOiJ3c3M6Ly93d3cucmVkdGVhbXNlYy54eXovd3MiLCJsb2NhbFBvcnQiOjg3NjV9fQo=" }
